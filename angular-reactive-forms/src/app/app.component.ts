5f1ff2b88322d24d955faafa1e51e5aa71b67b88
Shea Parrott <alexandergotthis@gmail.com>
2024-08-24 18:26:19 -0400

    I have achieved the goal and expanded upon it to show the user profiles created rather than simply storing it in state. Will update the README to capture this plus some new additions that will make this example solid.

--- angular-reactive-forms/src/app/app.component.ts
+++ angular-reactive-forms/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { userProfile } from './Models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-reactive-forms';
+  userData: FormGroup = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: this.formBuilder.group({
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', Validators.required]
+    })
+  })
+  createdUserProfiles: userProfile[] = []
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  submitForm(): void {
+    console.log('userData: ', this.userData.value)
+    this.createdUserProfiles.push({
+      firstName: this.userData.value.firstName,
+      lastName: this.userData.value.lastName,
+      street: this.userData.value.address.street,
+      city: this.userData.value.address.city,
+      state: this.userData.value.address.state,
+      zipCode: this.userData.value.address.zipCode,
+    })
+
+    this.userData.reset()
+  }
+
+  isInvalid(formControlName: string): boolean {
+    return !!this.userData.get(formControlName)?.invalid &&
+      !!(this.userData.get(formControlName)?.dirty ||
+        this.userData.get(formControlName)?.touched)
+  }
 }
